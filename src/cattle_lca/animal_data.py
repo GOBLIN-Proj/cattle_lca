9ebd5bed839227c1d5c23fc4eed2d26a628d0c79
Colm Duffy <colm.p.duffy@gmail.com>
2024-01-09 10:12:42 +0000

    added animal data class

---
+++ src/cattle_lca/animal_data.py
@@ -0,0 +1,79 @@
+
+class AnimalData:
+
+    @staticmethod
+    def get_animal_concentrate_amount(animal):
+        return animal.con_amount
+    
+    @staticmethod
+    def get_animal_concentrate_type(animal):
+        return animal.con_type
+    
+    @staticmethod
+    def get_animal_forage(animal):
+        return animal.forage
+    
+    @staticmethod
+    def get_animal_cohort(animal):
+        return animal.cohort
+    
+    @staticmethod
+    def get_animal_population(animal):
+        return animal.pop
+    
+    @staticmethod
+    def get_animal_weight(animal):
+        return animal.weight
+    
+    @staticmethod
+    def get_animal_daily_milk(animal):
+        return animal.daily_milk
+    
+    @staticmethod
+    def get_animal_year(animal):
+        return animal.year
+    
+    @staticmethod
+    def get_animal_grazing(animal):
+        return animal.grazing
+    
+    @staticmethod
+    def get_animal_t_outdoors(animal):
+        return animal.t_outdoors
+    
+    @staticmethod
+    def get_animal_t_indoors(animal):
+        return animal.t_indoors
+    
+    @staticmethod
+    def get_animal_sold(animal):
+        return animal.n_sold
+    
+    @staticmethod
+    def get_animal_bought(animal):
+        return animal.n_bought
+    
+    @staticmethod
+    def get_animal_t_stabled(animal):
+        return animal.t_stabled
+    
+    @staticmethod
+    def get_animal_mm_storage(animal):
+        return animal.mm_storage
+    
+    @staticmethod
+    def get_animal_daily_spreading(animal):
+        return animal.daily_spreading
+    
+    @staticmethod
+    def get_animal_wool(animal):
+        return animal.wool
+    
+    @staticmethod
+    def get_animal_ef_country(animal):
+        return animal.ef_country
+    
+    @staticmethod
+    def get_animal_farm_id(animal):
+        return animal.farm_id
+    
